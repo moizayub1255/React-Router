1705d8388b6b6a059286153b60768a00b7b0ced0
moizayub1255 <moizayub401@gmail.com>
2026-02-12 15:02:35 +0500

    home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+        <nav>
+            <Link to="/login">Login</Link>
+            <Link to="/about">About</Link>
+            <Link to="/profile">Profile</Link>
+            <Link to="/settings">Settings</Link>
+            <Link to="/dashboard">Dashboard</Link>
+        </nav>
+    </div>
   )
 }
 
